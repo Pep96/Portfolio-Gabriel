4e883f5b7db409f992859d15f7b372e6d278c73d
Pep96 <isabelle06pereira@gmail.com>
2026-06-20 15:38:17 -0300

    Add Instagram and LinkedIn profile links

--- lib/constants/sns_links.dart
+++ lib/constants/sns_links.dart
@@ -1,8 +1,8 @@
 class SnsLinks {
   static const String github = "https://github.com/Pep96";
-  static const String linkedIn = "https://";
+  static const String linkedIn = "https://www.linkedin.com/in/gabriel-pereira-8a0a35188/";
   static const String facebook = "https://";
-  static const String instagram = "https://";
+  static const String instagram = "https://www.instagram.com/devgabri/";
   static const String telegram = "https://";
   static const String blog = "https://";
 }
